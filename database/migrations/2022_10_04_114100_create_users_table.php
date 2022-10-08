b018034c9b4e85794d399c126137b0d31c62c600
Hammed Oyedele <itz.harmid@gmail.com>
2022-10-08 21:17:00 +0100

    feat: update database structure

--- database/migrations/2022_10_04_114100_create_users_table.php
+++ database/migrations/2022_10_04_114100_create_users_table.php
@@ -15,6 +15,7 @@ return new class() extends Migration {
     {
         Schema::create('users', function (Blueprint $table) {
             $table->uuid('id')->primary();
+            $table->uuid('shop_id')->nullable();
             $table->string('first_name');
             $table->string('last_name');
             $table->enum('type', UserType::values());
