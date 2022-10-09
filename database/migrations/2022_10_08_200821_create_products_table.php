64995c956005c0552e66214f2357640b884423ef
Hammed Oyedele <itz.harmid@gmail.com>
2022-10-09 20:20:02 +0100

    feat: add image field to products

--- database/migrations/2022_10_08_200821_create_products_table.php
+++ database/migrations/2022_10_08_200821_create_products_table.php
@@ -15,6 +15,7 @@ return new class() extends Migration {
         Schema::create('products', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('name');
+            $table->string('image')->nullable();
             $table->bigInteger('price')->default(0);
             $table->timestamps();
         });
