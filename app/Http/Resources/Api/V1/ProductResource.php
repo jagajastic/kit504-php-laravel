3b572467fba4bea40cd02d4ab9ff471e03d2a974
Hammed Oyedele <itz.harmid@gmail.com>
2022-10-10 12:10:49 +0100

    feat: add product resource data

--- app/Http/Resources/Api/V1/ProductResource.php
+++ app/Http/Resources/Api/V1/ProductResource.php
@@ -15,10 +15,12 @@ class ProductResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id'        => $this->id,
-            'name'      => $this->name,
-            'price'     => $this->price,
-            'price_usd' => $this->price_usd,
+            'id'           => $this->id,
+            'name'         => $this->name,
+            'price'        => $this->price,
+            'price_usd'    => $this->price_usd,
+            'created_at'   => $this->created_at,
+            'updated_at'   => $this->updated_at,
         ];
     }
 }
