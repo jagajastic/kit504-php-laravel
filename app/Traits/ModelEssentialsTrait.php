6eb261669e42dab3d72934c678451d8fba9cc5ed
Hammed Oyedele <itz.harmid@gmail.com>
2022-10-10 11:16:20 +0100

    feat: add model files support to trait

--- app/Traits/ModelEssentialsTrait.php
+++ app/Traits/ModelEssentialsTrait.php
@@ -2,8 +2,10 @@
 
 namespace App\Traits;
 
+use App\Casts\FileCast;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Throwable;
 
 /**
  * Essential methods and properties used by models.
@@ -24,12 +26,35 @@ trait ModelEssentialsTrait
      * Setup model event hooks.
      *
      * - Adds UUID to ID field for new item.
+     * - Deletes a model attached files after deleting the model.
      */
     public static function bootModelEssentialsTrait()
     {
         static::creating(function (Model $model) {
             if (!$model->getKey()) {
                 $model->{$model->getKeyName()} = Str::orderedUuid()->toString();
+            }
+        });
+
+        static::deleted(function (Model $model) {
+            try {
+                foreach ($model->getCasts() as $key => $value) {
+                    if (Str::startsWith($value, FileCast::class)) {
+                        $args = \explode(':', $value);
+
+                        if (\count($args) === 2) {
+                            $args = \explode(',', $args[1]);
+
+                            $fileCast = new FileCast(...$args);
+
+                            $fileCast->deleteOldFileIfExists(
+                                $model->getAttributes(),
+                                $key
+                            );
+                        }
+                    }
+                }
+            } catch (Throwable $e) {
             }
         });
     }
